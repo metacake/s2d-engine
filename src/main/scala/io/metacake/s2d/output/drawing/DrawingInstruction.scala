5be19dba7b3d3aff8758d8bba3a53d2b2dec29e6
Ryan Plessner <plessner.ryan@gmail.com>
2014-01-01 18:21:23 -0500

    added a getX and getY to the DrawingInstruction

--- src/main/scala/io/metacake/s2d/output/drawing/DrawingInstruction.scala
+++ src/main/scala/io/metacake/s2d/output/drawing/DrawingInstruction.scala
@@ -3,4 +3,8 @@ package io.metacake.s2d.output.drawing
 import io.metacake.core.output.RenderingInstruction
 import java.awt.Graphics2D
 
-abstract class DrawingInstruction extends RenderingInstruction[Graphics2D]
+abstract class DrawingInstruction extends RenderingInstruction[Graphics2D] {
+  def getX: Int
+
+  def getY: Int
+}
