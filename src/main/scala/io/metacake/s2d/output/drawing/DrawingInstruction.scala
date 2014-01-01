42a21c7114aca69593b706b0aef4fb0b7ddeccbf
Ryan Plessner <plessner.ryan@gmail.com>
2014-01-01 18:18:26 -0500

    actually added a DrawingInstruction

---
+++ src/main/scala/io/metacake/s2d/output/drawing/DrawingInstruction.scala
@@ -0,0 +1,6 @@
+package io.metacake.s2d.output.drawing
+
+import io.metacake.core.output.RenderingInstruction
+import java.awt.Graphics2D
+
+abstract class DrawingInstruction extends RenderingInstruction[Graphics2D]
