c26c4df81c245816e0f70fc4990d80aa6f48fa70
Ryan Plessner <plessner.ryan@gmail.com>
2014-05-13 20:23:53 -0400

    java doesn't seem to want to interop with scala package objects. fixed that by not using the package object

---
+++ src/main/scala/io/metacake/s2d/output/drawing/instructions/DrawInstruction.scala
@@ -0,0 +1,33 @@
+package io.metacake.s2d.output.drawing.instructions
+
+import io.metacake.core.output.RenderingInstruction
+import java.awt.{Rectangle, Shape, Color, Graphics2D}
+import java.awt.geom.Ellipse2D
+
+abstract class DrawInstruction extends RenderingInstruction[Graphics2D]
+
+class PlaceInstruction(image: DrawInstruction, x: Int, y: Int, base: DrawInstruction) extends DrawInstruction {
+  override def render(context: Graphics2D): Unit = {
+    base.render(context)
+    context.translate(x, y)
+    image.render(context)
+    context.translate(-x, -y)
+  }
+}
+
+abstract class ColoredDrawInstruction(color: Color) extends DrawInstruction {
+  override def render(context: Graphics2D) = context.setColor(color)
+}
+
+class FilledShapeInstruction(val shape: Shape, color: Color) extends ColoredDrawInstruction(color) {
+  override def render(context: Graphics2D): Unit = {
+    super.render(context)
+    context.fill(shape)
+  }
+}
+
+class CircleInstruction(radius: Int, color: Color)
+  extends FilledShapeInstruction(new Ellipse2D.Float(0, 0, radius, radius), color)
+
+class RectangleInstruction(width: Int, height: Int, color: Color)
+  extends FilledShapeInstruction(new Rectangle(0, 0, width, height), color)
